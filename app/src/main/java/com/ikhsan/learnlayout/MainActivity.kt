cb0c053fd507c52a763fc02fd621eeb3d145eb4a
Rahmad Noor Ikhsan <rajaikhsan2021@gmail.com>
2023-02-27 09:29:24 +0700

    Initial commit

---
+++ app/src/main/java/com/ikhsan/learnlayout/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ikhsan.learnlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Iphone 13 Pro Max"
+    }
+}
